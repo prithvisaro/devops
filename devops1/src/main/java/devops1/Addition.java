98915c53e5b6635942baf27908c95116c9abd13f
prithvisaro <70493148+prithvisaro@users.noreply.github.com>
2020-09-05 16:06:46 -0400

    changed the addition no

--- devops1/src/main/java/devops1/Addition.java
+++ devops1/src/main/java/devops1/Addition.java
@@ -5,7 +5,7 @@ public class Addition {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a = 10;
-		int b = 20;
+		int b = 30;
 		int c= a+b;
 		System.out.println("Addition of a & b :" + c);
 	}
