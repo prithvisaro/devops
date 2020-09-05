3c2303d5c60fac6a0f4e9e8556abd9b73935ef98
prithvisaro <70493148+prithvisaro@users.noreply.github.com>
2020-09-05 15:42:35 -0400

    Added new function to multiply

--- devops1/src/main/java/devops1/Multiplication.java
+++ devops1/src/main/java/devops1/Multiplication.java
@@ -1,15 +1,41 @@
 package devops1;
 
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class Multiplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		// TODO Auto-generated method stub
-		int a = 10;
-		int b = 40;
-		int c= a*b;
-		System.out.println("Multiplication of a & b :" + c);
+        /* This reads the input provided by user
+         * using keyboard
+         */
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+
+        // This method reads the number provided using keyboard
+        int num1 = scan.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int num2 = scan.nextInt();
+
+        // Closing Scanner after the use
+        scan.close();
+        
+        int productvalue = multiply(num1,num2);
+        
+        System.out.println("Multiply Values :" + productvalue);
+        
+        
+ 
+	}
+	
+	static int multiply(int num1, int num2)
+	{
+	       // Calculating product of two numbers
+        int product = num1*num2;
+        
+        
+        return product;
 
 	}
 
