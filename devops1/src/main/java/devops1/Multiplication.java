d4a3efa1212ec4a182ad4abd430dced649c5928b
prithvisaro <70493148+prithvisaro@users.noreply.github.com>
2020-09-05 15:33:32 -0400

    modified multiplication

--- devops1/src/main/java/devops1/Multiplication.java
+++ devops1/src/main/java/devops1/Multiplication.java
@@ -7,7 +7,7 @@ public class Multiplication {
 		
 		// TODO Auto-generated method stub
 		int a = 10;
-		int b = 30;
+		int b = 40;
 		int c= a*b;
 		System.out.println("Multiplication of a & b :" + c);
 
