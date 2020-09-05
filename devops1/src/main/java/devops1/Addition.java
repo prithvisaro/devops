a5998f19766b7a19a7271c6dacfe97fb7a1a37cf
prithvisaro <70493148+prithvisaro@users.noreply.github.com>
2020-09-05 15:26:22 -0400

    changed to print demo text

---
+++ devops1/src/main/java/devops1/Addition.java
@@ -0,0 +1,13 @@
+package devops1;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		int c= a+b;
+		System.out.println("Addition of a & b :" + c);
+	}
+
+}
