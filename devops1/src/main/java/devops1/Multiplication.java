17f53bebea380daa7f709bc3524450d7872812a5
prithvisaro <70493148+prithvisaro@users.noreply.github.com>
2020-09-05 15:31:25 -0400

    Multiplication

---
+++ devops1/src/main/java/devops1/Multiplication.java
@@ -0,0 +1,16 @@
+package devops1;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		int c= a*b;
+		System.out.println("Multiplication of a & b :" + c);
+
+	}
+
+}
