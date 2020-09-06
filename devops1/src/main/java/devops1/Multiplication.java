39ea6877af98fba0f062a827f37b73d499dfbe78
prithvisaro <70493148+prithvisaro@users.noreply.github.com>
2020-09-06 11:55:26 -0400

    Changed to substract

--- devops1/src/main/java/devops1/Multiplication.java
+++ devops1/src/main/java/devops1/Multiplication.java
@@ -8,19 +8,8 @@ public class Multiplication {
 
 	public int multiply(int num1, int num2)
 	{
-//		   Scanner scan = new Scanner(System.in);
-//	        System.out.print("Enter first number: ");
-//
-//	        // This method reads the number provided using keyboard
-//	        int num1 = scan.nextInt();
-//	        
-//	        System.out.print("Enter second number: ");
-//	        int num2 = scan.nextInt();
-//
-//	        // Closing Scanner after the use
-//	        scan.close();
-	        
-	        int productvalue = num1*num2;
+			//Changed the value to -
+	        int productvalue = num1-num2;
 	        
 	        System.out.println("Multiply Values :" + productvalue);
 	        
